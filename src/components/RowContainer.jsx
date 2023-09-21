0fbfa0eb741b1a010ba72845fff69ce5518916aa
Jeetesh <jeeteshkumar45@gmail.com>
2023-09-22 01:25:00 +0530

    fruits-Part3

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,42 +1,68 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
+import NotFound from "../assets/img/NotFound.svg";
 
-const RowContainer = ({ flag, data }) => {
+const RowContainer = ({ flag, data, scrollValue }) => {
   console.log(data);
+
+  const rowContainer = useRef();
+  useEffect(() => {
+    rowContainer.current.scrollLeft += scrollValue;
+  }, [scrollValue]);
   return (
     <div
-      className={`w-full my-12 bg-rowBg   ${
-        flag ? "overflow-x-scroll" : "overflow-x-hidden"
+      ref={rowContainer}
+      className={`w-full my-12 bg-rowBg  flex items-center gap-3 scroll-smooth  ${
+        flag
+          ? "overflow-x-scroll scrollbar-none"
+          : "overflow-x-hidden flex-wrap"
       }`}
     >
-      <div className="w-300 md:w-340 h-auto bg-cardOverlay rounded-lg backdrop-blur-lg hover:drop-shadow-lg p-2 my-12">
-        <div className="w-full flex items-center justify-between overflow-hidden transition-all ease-in duration-100">
-          <motion.img
-            whileHover={{ scale: 1.25 }}
-            src="https://lh3.googleusercontent.com/a/ACg8ocLZpitAaVN8V3htarsPw1yB51GoUPzaV706HOwlCIaHEg=s96-c"
-            alt="fruits"
-            className="w-40 -mt-8 drop-shadow-2xl"
-          />
-          <motion.div
-            whileTap={{ scale: 0.75 }}
-            className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md"
+      {data && data.length > 0 ? (
+        data.map((item) => (
+          <div
+            // key={item.id}
+            className="w-300 min-w-[300px] md:w-340 md:min-w[340px] h-auto bg-cardOverlay rounded-lg backdrop-blur-lg hover:drop-shadow-lg p-2 my-12"
           >
-            <MdShoppingBasket className="text-white" />
-          </motion.div>
-        </div>
-        <div className="w-full flex flex-col items-end justify-end">
-          <p className="text-lg text-textColor font-semibold">
-            Chocolate & Vanilla
-          </p>
-          <p className="mt-1 text-sm text-gray-500">45 Calories</p>
-          <div className="flex items-center gap-8">
-            <p className="text-lg text-headingColor font-semibold">
-              <span className="text-sm text-red-500">$</span>5.25
-            </p>
+            <div className="w-full flex items-center justify-between overflow-hidden transition-all ease-in duration-100">
+              <motion.img
+                whileHover={{ scale: 1.25 }}
+                src={item?.imageURL}
+                alt="fruits"
+                className="w-40 -mt-8 drop-shadow-2xl object-contain"
+              />
+              <motion.div
+                whileTap={{ scale: 0.75 }}
+                className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md"
+              >
+                <MdShoppingBasket className="text-white" />
+              </motion.div>
+            </div>
+            <div className="w-full flex flex-col items-end justify-end">
+              <p className="text-lg text-textColor font-semibold">
+                {item?.title}
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                {item?.calories}Calories
+              </p>
+              <div className="flex items-center gap-8">
+                <p className="text-lg text-headingColor font-semibold">
+                  <span className="text-sm text-red-500">$</span>
+                  {item?.price}
+                </p>
+              </div>
+            </div>
           </div>
+        ))
+      ) : (
+        <div className="w-full flex flex-col items-center justify-center">
+          <img src={NotFound} className="h-340" alt="not found" />
+          <p className="text-xl text-headingColor font-semibold my-2">
+            Items Not Available
+          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
